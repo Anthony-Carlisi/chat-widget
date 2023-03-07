531acacf077be4fca5b1f0be6fd41eec431cb3c7
Anthony-Carlisi <93996498+Anthony-Carlisi@users.noreply.github.com>
2023-03-06 22:11:33 -0500

    update

---
+++ src/components/Form.js
@@ -0,0 +1,121 @@
+import '../App.css'
+import { useState } from 'react'
+import {
+  minMaxLength,
+  numericOnly,
+  lettersOnly,
+  phoneValue,
+} from '../utils/Validations'
+import { useNavigate } from 'react-router-dom'
+
+const Form = () => {
+  const navigate = useNavigate()
+  const [form, setForm] = useState({})
+  const [formErrors, setFormErrors] = useState({
+    name: '',
+    phone: '',
+    question: '',
+  })
+
+  const handleChange = (e) => {
+    const name = e.target.name
+    let value = e.target.value
+    let error = ''
+    // Switch for field names
+    switch (name) {
+      case 'name':
+        value = lettersOnly(value)
+        break
+
+      case 'phone':
+        value = numericOnly(value)
+        if (minMaxLength(value, 10)) error = 'Enter valid phone number'
+        value = phoneValue(value)
+        break
+
+      default:
+        break
+    }
+    //Global validation checks
+    if (minMaxLength(value, 1)) {
+      error = 'Cannot be empty'
+    }
+    // Sets Errors
+    if (error) setFormErrors({ ...formErrors, [name]: error })
+    // Deletes Errors
+    if (!error && formErrors.hasOwnProperty(name)) delete formErrors[name]
+    // Sets Values
+    setForm({ ...form, [name]: value })
+  }
+
+  const autosize = (e) => {
+    const { currentTarget } = e
+    setTimeout(() => {
+      currentTarget.style = 'height:auto;padding:0'
+      currentTarget.style = 'height:' + currentTarget.scrollHeight + 'px'
+    }, 0)
+  }
+
+  const handleSubmit = () => {
+    navigate('/chat', { message: form.question })
+  }
+
+  return (
+    <div>
+      <div className='flex-column'>
+        <label className='input-label' htmlFor='phone'>
+          Name
+        </label>
+        <input
+          className='input'
+          type='text'
+          name='name'
+          onChange={handleChange}
+          value={form.name || ''}
+        />
+        <p className='input-error'>{formErrors.name}</p>
+      </div>
+      {/* Phone */}
+      <div className='flex-column'>
+        <label className='input-label' htmlFor='phone'>
+          Phone
+        </label>
+        <input
+          className='input'
+          type='text'
+          name='phone'
+          onChange={handleChange}
+          value={form.phone || ''}
+        />
+        <p className='input-error'>{formErrors.phone}</p>
+      </div>
+      {/* Message */}
+      <div className='flex-column'>
+        <label className='input-label' htmlFor='question'>
+          Message
+        </label>
+        <textarea
+          className='textarea'
+          rows='1'
+          type='text'
+          name='question'
+          onChange={handleChange}
+          onKeyDown={autosize}
+          value={form.question || ''}
+        ></textarea>
+        <p className='input-error'>{formErrors.question}</p>
+      </div>
+      <div className='flex-center'>
+        <button
+          className='input-button flex-center'
+          disabled={Object.keys(formErrors).length !== 0}
+          onClick={handleSubmit}
+        >
+          <h3> Start Chat</h3>
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default Form
---
+++ src/components/Widget.js
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import '../App.css'
+import { ChatIcon, DownArrow } from '../assets/Icons'
+import Form from './Form'
+
+const Widget = () => {
+  const [isActive, setIsActive] = useState(true)
+
+  const handleToggle = () => {
+    setIsActive(!isActive)
+  }
+
+  return (
+    <div className='fixed'>
+      {/* Chat box start */}
+      <div
+        onClick={handleToggle}
+        className={isActive ? 'chat-icon-wrapper' : 'no-display'}
+      >
+        <h3 className='chat-icon-text'>Chat</h3>
+        <ChatIcon className='chat-icon' alt='chat-icon' />
+      </div>
+
+      {/* Chat header */}
+      <div className={isActive ? 'no-display' : 'chat-window-container'}>
+        <div className='chat-window-header'>
+          <h2 className='chat-window-header-text'> Get in touch</h2>
+          <DownArrow
+            className='chat-window-header-icon'
+            alt='chat-icon-downarrow'
+            onClick={handleToggle}
+          />
+        </div>
+        {/* Chat Body */}
+        <Form />
+      </div>
+    </div>
+  )
+}
+
+export default Widget
